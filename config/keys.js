4f95cce208c67986011074889562733b9ad36c38
Jeromeski <jeromegee@gmail.com>
2020-02-20 06:31:19 +0800

    Passport JWT Authentication Strategy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://jeromegee:belowsubzero@devconnector-gchx2.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://jeromegee:belowsubzero@devconnector-gchx2.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'secret' 
+
 }
